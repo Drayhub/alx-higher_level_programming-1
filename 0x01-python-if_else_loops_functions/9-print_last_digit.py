5d0be2d872ea281c1252f468830417efa2ef1f4d
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-12-01 00:11:53 +0100

    fixed issue with return

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
+""" #!/usr/bin/python3 """
 def print_last_digit(number):
     print("{}".format(number % 10), end="")
+    return number % 10
+r = print_last_digit(98)
+print(r)
