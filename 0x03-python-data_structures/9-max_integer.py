d40c627af63b6a881d275162f05a3f3ca0dfe82f
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-12-05 20:33:25 +0100

    account for -ve numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,10 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        for i in range(len(my_list) - 1):
-            j = i + 1
-            max = my_list[i] if my_list[i] > my_list[j] else my_list[j]
-        return max
+        max_int = my_list[0]
+        for i in my_list:
+            if i > max_int:
+                max_int = i
+        return max_int
+list = [1, 90, 2, 13, 34, -13, 5, 3]
+print(max_integer(list))
