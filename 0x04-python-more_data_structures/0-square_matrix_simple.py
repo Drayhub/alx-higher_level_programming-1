6ca19622c5b90961f4fe5183cf3fa510056c8b4c
Bappa-Kamba <bappahmuhammad18@gmail.com>
2022-12-10 00:25:05 +0100

    bhu

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    ridx = 0
-    for row in new_matrix:
-        cidx = 0
-        for num in row:
-            matrix[ridx][cidx] = num ** 2
-            cidx += 1
-        ridx += 1
-    return new_matrix
+    if matrix is not None:
+        new = []
+        for rows in matrix:
+            new.append(list(map(lambda x: x**2, rows)))
+        return (new)
+    return None
